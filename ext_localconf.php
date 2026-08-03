6f23f28ebea3be788d766c3b08f5078797b69a6f
Simon Schmidt <simon.schmidt@typo3.com>
2026-08-03 16:36:57 +0200

    [TASK] exclude live tracking params from cHash generation

--- ext_localconf.php
+++ ext_localconf.php
@@ -45,4 +45,10 @@ call_user_func(static function (): void {
         'backend' => \TYPO3\CMS\Core\Cache\Backend\Typo3DatabaseBackend::class,
         'options' => ['defaultLifetime' => $pageAnalyticsCacheTtl > 0 ? $pageAnalyticsCacheTtl : 3600],
     ];
+
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'visa_auto_track_live_preview';
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'locale';
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'from';
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'until';
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'domPathKey';
 });
